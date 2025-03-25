359211bb71a4d3215f9eb35a970865161e44a6fe
nasa01-crypto <natalie.samaan@live.se>
2025-03-25 13:31:01 +0100

    Initial Commit

---
+++ SpeedWriterTests/SpeedWriterTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedWriterTests.swift
+//  SpeedWriterTests
+//
+//  Created by Natalie S on 2025-03-25.
+//
+
+import Testing
+@testable import SpeedWriter
+
+struct SpeedWriterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
